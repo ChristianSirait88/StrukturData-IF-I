0a740b939cc7cacb44406b0c9d89905f03aab244
ChristianSirait88 <christiansirait88@gmail.com>
2023-09-15 13:21:57 +0700

    pertemuan 2 yang benar

--- pertemuan 2/pertemuan2.cpp
+++ pertemuan 2/pertemuan2.cpp
@@ -1,120 +1,125 @@
-/*Program List Dengan Array*/
+/* Implementasi Linked List dengan pointer */
 #include <iostream>
-#define max 12
+#include <malloc.h>
 #define true 1
 #define false 0
-using namespace std;
-typedef struct {int info;	 int next;	}typenode;
-typenode elemen[max];
-int list,akhirlist,kosong,akhirkosong;
+using std::cout;
+
+typedef int typeinfo;
+typedef struct typenode *typeptr;
+typedef struct typenode{typeinfo info;
+typeptr next;};
+typeptr awal,akhir;
+void buatlistbaru();
+void sisipnode(typeinfo IB);
+void hapusnode(typeinfo IH);
+void bacamaju();
+void bacamundur();
 int listkosong();
-void buatlist();
-void sisipnode(int IB);
-void hapusnode(int IH);
-void cetaklist();
-
 int main()
-{ buatlist();
-  cetaklist();
-  sisipnode(7);
-  cout << "\n\n Sisip 7 \n\n";
-  cetaklist();
-//   sisipnode(30);
-//   cout << "\n\n sisip 30 \n\n";
-//   cetaklist();
-//   hapusnode(7);
-//   cout << "\n\n hapus 7 \n\n";
-//   cetaklist();
+{
+  cout << "List Mula-mula :\n";
+  buatlistbaru();
+  sisipnode(50);
+  sisipnode(20);
+  sisipnode(5);
+  sisipnode(100);
+  sisipnode(70);
+  sisipnode(25);
+  bacamaju();
+  
+  cout << "\n\nHapus node 50\n";
+  cout << "\nKodisi List setelah di hapus, dibaca dari belakang :\n\n";
+  hapusnode(50);
+  bacamundur();
+  return 0;
 }
-void buatlist()
-{ list=5;
-kosong=3;
-akhirlist=10;
-akhirkosong=4;
-elemen[1].info=25;	elemen[1].next=8;
-elemen[2].info=0 ;	elemen[2].next=9;
-elemen[3].info=0; 	elemen[3].next=6;
-elemen[4].info=0; 	elemen[4].next=0;
-elemen[5].info=10;	elemen[5].next=7;
-elemen[6].info=0; 	elemen[6].next=2;
-elemen[7].info=15;	elemen[7].next=1;
-elemen[8].info=40;	elemen[8].next=10;
-elemen[9].info=0;	   elemen[9].next=4;
-elemen[10].info=60;	elemen[10].next=0;
+void buatlistbaru()
+{
+  typeptr list;
+  list=NULL;
+  awal=list;
+  akhir=list;
 }
+
 int listkosong()
-{ if (list==0)
-	  return(true);
+{ if(awal==NULL)
+	 return(true);
   else
-	  return(false);
-}
-void sisipnode(int IB)
-{ int listbaru,k,m,n,x;
-//-------------------------- sisip di awal
-  if(IB<elemen[list].info)
-  {listbaru=kosong;
-	kosong=elemen[kosong].next;
-	elemen[listbaru].info=IB;
-	elemen[listbaru].next=list;
-	list=listbaru;
-  }
+	 return(false); }
 
-else
-  //-------------------------sisip di akhir
-  if(IB>elemen[akhirlist].info)
-  {listbaru=kosong;
-	kosong=elemen[kosong].next;
-	elemen[listbaru].info=IB;
-	elemen[listbaru].next=0;
-	elemen[akhirlist].next=listbaru;
-	akhirlist=listbaru;
-  }
+void sisipnode(typeinfo IB)
+{ typeptr NB, bantu;
+  NB=(typenode *) malloc(sizeof(typenode));
+  NB->info=IB;
+  NB->next=NULL;
+  if (listkosong())
+  { awal=NB;
+	 akhir=NB; }
+  else if (IB <= awal->info)       // Sisip di depan
+	 { NB->next=awal;
+		awal=NB; }
   else
-  //--------------------------sisip di tengah
-  {n=list;
-	x=elemen[n].info;
-	while(IB>x)
-	{m=n;
-	 n=elemen[n].next;
-	 x=elemen[n].info;
-	}
-	k=elemen[kosong].next;
-	elemen[m].next=kosong;
-	elemen[kosong].info=IB;
-	elemen[kosong].next=n;
-	kosong=k;
-	}
-  }
-void cetaklist()
-{ int n,m;
-  n=list;
-  m=kosong;
-  cout << "isi list : \n";
-do{
-  cout << elemen[n].info << " ";
-  n=elemen[n].next;
-  }while (elemen[n].next!=0);
-  cout << " "<< elemen[akhirlist].info <<endl ;
-  cout << "\nIndex tempat-tempat kosong: \n";
-  do
-  {cout << m << " ";
-	m=elemen[m].next;
-  }while(elemen[m].next!=0);
-  cout << " " << akhirkosong;
+	 { bantu=awal;
+		while (bantu->next!=NULL && IB > bantu->next->info)
+			  bantu=bantu->next;
+		NB->next=bantu->next;  // Sisip di tengah atau di belakang
+		bantu->next=NB;
+		if (IB>akhir->info)
+			akhir=NB;
+	 }
 }
 
-void hapusnode(int IH)
-{int listbaru,k;
- //------------------------ hapus di awal
- if(IH=elemen[list].info)
- {listbaru=elemen[list].next;
-  k=kosong;
-  kosong=list;
-  list=listbaru;
-  elemen[kosong].next=k;
+void hapusnode(typeinfo IH)
+{ typeptr hapus, bantu;
+  if (listkosong())
+	 cout << "List masih kosong";
+  else if (awal->info==IH)
+  { hapus=awal;                      // Hapus di awal
+	 awal=hapus->next;
+	 free(hapus); }
+  else
+  { bantu=awal;
+	 while (bantu->next->next!=NULL && IH!=bantu->next->info)
+		 bantu=bantu->next;
+	 if (IH==bantu->next->info)
+		 { hapus=bantu->next;
+			if (hapus==akhir)             // Hapus di akhir
+			  { akhir=bantu;
+				 akhir->next=NULL; }
+			else
+				 bantu->next=hapus->next;   // Hapus di tengah
+			free(hapus);
+		  }
+	 else
+		cout << "Node tidak ditemukan!\n";  }
+}
+void bacamaju()
+{
+  typeptr bantu;
+  bantu=awal;
+  while (bantu!=NULL)
+  {
+	 cout << " " << bantu->info;
+	 cout << "  ";
+	 bantu=bantu->next;
   }
-  //------------------hapus di akhir
-  //       buat sendiri!! untuk TUGAS
-  //------------------hapus di tengah
-  //       buat sendiri!! untuk TUGAS
-  }
+}
+void bacamundur()
+{ typeptr depan,bantu;
+  depan=awal;
+  awal=akhir;
+  do { bantu=depan;
+	 while (bantu->next!=akhir)
+		  bantu=bantu->next;
+	 akhir->next=bantu;
+	 akhir=bantu;
+	 } while (akhir!=depan);
+  akhir->next=NULL;
+  bantu=awal;
+  while (bantu!=NULL)
+  { cout << " " << bantu->info;
+	 cout << "  ";
+	 bantu=bantu->next;  }
+}
+
